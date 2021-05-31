b3e8874c5b25744acf5d330343614e5f8765b5b5
Daniel Fang <daniel.fangtz@gmail.com>
2021-05-31 00:01:59 -0700

    database access, needs to use EF6 instead of dumbass dapper

--- AdminFeeSetting.cs
+++ AdminFeeSetting.cs
@@ -1,24 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace BookQuote
 {
-    static class AdminFeeSetting
+    public class AdminFeeSetting
     {
         //needs to add some form of credential validation 
-        static public double sewingSetupFee { get; set; }
-        static public double sig4Cost { get; set; }
-        static public double sig8Cost { get; set; }
-        static public double sig12Cost { get; set; }
-        static public double sig16Cost { get; set; }
-        static public double caseInSetupFee { get; set; }
-        static public double caseInPerBookCost { get; set; }
-        static public double perfectBindSetupFee { get; set; }
-        static public double perfectBindPerBookCost { get; set; }
-        static public double endSheetSetupFee { get; set; }
-        static public double endSheetPerBookCost { get; set; }
-        static public double caseBindingSetupFee { get; set; }
-        static public double caseBindingPerBookCost { get; set; }
+        
+        public int ID { get; set; }
+        public double sewingSetupFee { get; set; }
+        public double sig4Cost { get; set; }
+        public double sig8Cost { get; set; }
+        public double sig12Cost { get; set; }
+        public double sig16Cost { get; set; }
+        public double caseInSetupFee { get; set; }
+        public double caseInPerBookCost { get; set; }
+        public double perfectBindSetupFee { get; set; }
+        public double perfectBindPerBookCost { get; set; }
+        public double endSheetSetupFee { get; set; }
+        public double endSheetPerBookCost { get; set; }
+        public double caseBindingSetupFee { get; set; }
+        public double caseBindingPerBookCost { get; set; }
     }
 }
---
+++ SqliteAccess.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+
+namespace BookQuote
+{
+    public class SqliteAccess
+    { 
+        public static AdminFeeSetting LoadAdminFees()
+        {
+            //open database connection and close once using block finish execution
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var adminfees = cnn.Query<AdminFeeSetting>("select * from AdminFeeSetting Where ID=0", new DynamicParameters());
+                return (AdminFeeSetting)adminfees;
+            }
+
+        }
+
+        public static void SaveAdminFees(AdminFeeSetting adminfeesetting)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString())) {
+
+            }
+        }
+        private static string LoadConnectionString(string id = "Default")
+        {//pass in default connection string
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+
+    }
+}
